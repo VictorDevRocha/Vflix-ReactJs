8534ff2b4dd28322de86af8c374331b1c55c4b9f
Victor Rocha <victorrochadev0@gmail.com>
2022-08-31 23:32:50 -0300

    Estilização do styled components, criação da pasta Key e adicionando a variavel APIKey onde fica minha chave da API

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Container } from "./style";
+
+function Home() {
+
+  const [movies, setMovies] = useState([])
+  return <Container>
+          <h1>Hello World</h1>
+          </Container>
+}
+
+export default Home;
