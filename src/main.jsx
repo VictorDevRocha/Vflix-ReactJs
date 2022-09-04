d1f4db13408f711b8728c3564e7318aef5b53379
Victor Rocha <victorrochadev0@gmail.com>
2022-09-04 19:21:28 -0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { GlobalElement } from './pages/common/'
-import Details from './pages/Details'
-import Home from './pages/Home'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalElement } from "./pages/common";
+import Details from "./pages/Details";
+import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <GlobalElement/>
+    <GlobalElement />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<Details />} />
     </Routes>
   </BrowserRouter>
-)
+);
